1c62c0d4b32d2176f1eba3864e770158b064a035
okarinbro <prz.jab98@gmail.com>
2019-05-03 23:08:55 +0200

    OCR class in progress

---
+++ OpticalCharacterRecognition.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import scipy
+from scipy import ndimage
+
+
+class OpticalCharacterRecognition:
+    def __init__(self, file_path):
+        # invert image!
+        self.image = scipy.ndimage.imread(file_path, mode='L')
+        # TO DO -> load patterns
+        self.letters_positions = {}
+
+    def get_correlation(self, image, pattern):
+        return np.real(
+            np.abs(np.fft.ifft2(np.multiply(np.fft.fft2(image), np.fft.fft2(np.rot90(pattern, 2), image.shape)))))
+
+    def print_image(self, image):
+        plt.imshow(image)
+        plt.show()
+
+
